5f7ead535888751b7d289b55415a0bc61c3be79a
transfix <transfix@sublevels.net>
2016-03-09 22:01:38 -0600

    Modifying webapi keepalive script for new module. Related to issue #3

---
+++ atxcf/__main__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import webapi
+
+webapi.main()
--- webapi.sh
+++ webapi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-until python webapi.py; do
-    echo "Server 'webapi.py' crashed with exit code $?.  Respawning.." >&2
+until /usr/bin/python -m atxcf; do
+    echo "Server atxcf crashed with exit code $?.  Respawning.." >&2
     sleep 1
 done
